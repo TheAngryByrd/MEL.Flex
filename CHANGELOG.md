2d3b2327f615a586dd3c86d60fce730a28bcd4ec
Jimmy Byrd <jimmybyrd@gmail.com>
2022-04-08 16:21:02 -0400

    Bump version to 0.1.0-beta001
    ## [0.1.0-beta001] - 2022-04-08
    
    ### Added
    
    - Tuple Interpolation Support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,15 +1,23 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [0.1.0-beta001] - 2022-04-08
 
-## [0.1.0] - 2017-03-17
+### Added
+
+- Tuple Interpolation Support
+
+## [0.0.0] - 2017-03-17
+
 First release
 
 ### Added
 - This release already has lots of features
 
-[0.1.0]: https://github.com/user/MEL.Flex.git/releases/tag/v0.1.0
+[Unreleased]: https://github.com/TheAngryByrd/MEL.Flex/compare/v0.1.0-beta001...HEAD
+[0.1.0-beta001]: https://github.com/TheAngryByrd/MEL.Flex/compare/v0.0.0...v0.1.0-beta001
+[0.1.0]: https://github.com/TheAngryByrd/MEL.Flex.git/releases/tag/v0.1.0
--- src/MEL.Flex/AssemblyInfo.fs
+++ src/MEL.Flex/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("MEL.Flex")>]
 [<assembly: AssemblyProductAttribute("MEL.Flex")>]
 [<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2022-04-08T00:00:00.0000000-04:00")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","beta")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","ee147f8096ff4c9bb7a1167bae855849823a77f2")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "MEL.Flex"
     let [<Literal>] AssemblyProduct = "MEL.Flex"
     let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2022-04-08T00:00:00.0000000-04:00"
     let [<Literal>] AssemblyFileVersion = "0.1.0"
     let [<Literal>] AssemblyInformationalVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "beta"
+    let [<Literal>] AssemblyMetadata_GitHash = "ee147f8096ff4c9bb7a1167bae855849823a77f2"
