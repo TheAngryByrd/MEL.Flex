19056afce7b39d507f7d99aa10cd36fbdd623f27
Jimmy Byrd <jimmybyrd@gmail.com>
2022-04-07 21:17:39 -0400

    Formatting

--- src/MEL.Flex/MEL.Flex.fs
+++ src/MEL.Flex/MEL.Flex.fs
@@ -1,4 +1,5 @@
 namespace MEL.Flex
+
 open System
 
 
@@ -10,14 +11,16 @@ module Tuple =
     open System.Collections.Generic
     open Microsoft.Extensions.Logging
 
-    let [<Literal>] OriginalFormat = "{OriginalFormat}"
+    [<Literal>]
+    let OriginalFormat = "{OriginalFormat}"
 
-    let internal setMessageInterpolated (message: FormattableString) : obj array  * string array * obj array =
+    let internal setMessageInterpolated (message: FormattableString) : obj array * string array * obj array =
         let msgArgs = message.GetArguments()
         let names = Array.zeroCreate msgArgs.Length
         let namesTemplate = Array.zeroCreate msgArgs.Length
         let args = Array.zeroCreate msgArgs.Length
-        for i = 0 to msgArgs.Length-1 do
+
+        for i = 0 to msgArgs.Length - 1 do
             match msgArgs.[i] with
             | :? ITuple as t when t.Length = 2 ->
                 names.[i] <- $"@{t.Item(0)}"
@@ -27,44 +30,42 @@ module Tuple =
                 names.[i] <- $"@item{i}"
                 namesTemplate.[i] <- box $"{{{names.[i]}}}"
                 args.[i] <- other
+
         namesTemplate, names, args
 
     // TODO: Convert to struct
     // [<Struct>]
-    type internal TupleLogValuesFormatter(formattable : FormattableString) =
+    type internal TupleLogValuesFormatter(formattable: FormattableString) =
 
         let _count = formattable.ArgumentCount + 1
-        let results = lazy (
-            // printfn "TupleLogValuesFormatter.results"
-            setMessageInterpolated formattable
-            )
-        let namesTemplate = lazy (
-            let (namesTemplate, _, _) = results.Value
-            namesTemplate
-        )
-        let names = lazy(
-            let (_,  names, _) = results.Value
-            names
-        )
-        let args = lazy(
-            let (_, _, args) = results.Value
-            args
-        )
-        let messageFormat = lazy (
-            String.Format(formattable.Format, namesTemplate.Value)
-        )
-        let formattedMessage = lazy (
-            String.Format(formattable.Format, args.Value)
-        )
-        member this.Names
-            with get () =
-                names.Value
-        member this.Args
-            with get () =
-                args.Value
-        member this.MessageTemplate
-            with get () =
-                messageFormat.Value
+
+        let results =
+            lazy
+                (
+                // printfn "TupleLogValuesFormatter.results"
+                setMessageInterpolated formattable)
+
+        let namesTemplate =
+            lazy
+                (let (namesTemplate, _, _) = results.Value
+                 namesTemplate)
+
+        let names =
+            lazy
+                (let (_, names, _) = results.Value
+                 names)
+
+        let args =
+            lazy
+                (let (_, _, args) = results.Value
+                 args)
+
+        let messageFormat = lazy (String.Format(formattable.Format, namesTemplate.Value))
+        let formattedMessage = lazy (String.Format(formattable.Format, args.Value))
+        member this.Names = names.Value
+        member this.Args = args.Value
+        member this.MessageTemplate = messageFormat.Value
+
         override this.ToString() =
 
             // printfn "TupleLogValuesFormatter.ToString"
@@ -72,88 +73,107 @@ module Tuple =
 
         interface IReadOnlyList<KeyValuePair<string, obj>> with
             member this.Item
-                with get(index : int) : KeyValuePair<string, obj> =
+                with get (index: int): KeyValuePair<string, obj> =
                     // printfn "TupleLogValuesFormatter.get"
                     if index < 0 || index >= _count then
-                        raise (IndexOutOfRangeException(nameof(index)))
-                    elif (index = (_count - 1) ) then
-                        KeyValuePair<string, obj> (OriginalFormat, this.MessageTemplate)
+                        raise (IndexOutOfRangeException(nameof (index)))
+                    elif (index = (_count - 1)) then
+                        KeyValuePair<string, obj>(OriginalFormat, this.MessageTemplate)
                     else
                         KeyValuePair<string, obj>(this.Names.[index], this.Args.[index])
-            member this.Count with get() : int = _count
-            member this.GetEnumerator() : IEnumerator<KeyValuePair<string,obj>> =
+
+            member this.Count: int = _count
+
+            member this.GetEnumerator() : IEnumerator<KeyValuePair<string, obj>> =
 
                 // printfn "TupleLogValuesFormatter.GetEnumerator"
                 (seq {
-                    for i=0 to _count-1 do
+                    for i = 0 to _count - 1 do
                         (this :> IReadOnlyList<_>)[i]
-                }).GetEnumerator()
+                })
+                    .GetEnumerator()
+
             member this.GetEnumerator() : IEnumerator =
-                (this:>IEnumerable<_>).GetEnumerator():>IEnumerator
+                (this :> IEnumerable<_>).GetEnumerator() :> IEnumerator
 
     type ILogger with
-        member this.LogITrace(eventId : EventId, ex : Exception,  message : FormattableString) =
+        member this.LogITrace(eventId: EventId, ex: Exception, message: FormattableString) =
             this.LogI(LogLevel.Trace, eventId, ex, message)
-        member this.LogITrace(eventId : EventId, message : FormattableString) =
+
+        member this.LogITrace(eventId: EventId, message: FormattableString) =
             this.LogI(LogLevel.Trace, eventId, message)
-        member this.LogITrace(ex : Exception, message : FormattableString) =
-            this.LogI(LogLevel.Trace, ex, message)
-        member this.LogITrace(message : FormattableString) =
-            this.LogI(LogLevel.Trace, message)
 
-        member this.LogIDebug(eventId : EventId, ex : Exception,  message : FormattableString) =
+        member this.LogITrace(ex: Exception, message: FormattableString) = this.LogI(LogLevel.Trace, ex, message)
+        member this.LogITrace(message: FormattableString) = this.LogI(LogLevel.Trace, message)
+
+        member this.LogIDebug(eventId: EventId, ex: Exception, message: FormattableString) =
             this.LogI(LogLevel.Debug, eventId, ex, message)
-        member this.LogIDebug(eventId : EventId, message : FormattableString) =
-            this.LogI(LogLevel.Debug, eventId, message)
-        member this.LogIDebug(ex : Exception, message : FormattableString) =
-            this.LogI(LogLevel.Debug, ex, message)
-        member this.LogIDebug(message : FormattableString) =
-            this.LogI(LogLevel.Debug, message)
 
-        member this.LogIInformation(eventId : EventId, ex : Exception,  message : FormattableString) =
+        member this.LogIDebug(eventId: EventId, message: FormattableString) =
+            this.LogI(LogLevel.Debug, eventId, message)
+
+        member this.LogIDebug(ex: Exception, message: FormattableString) = this.LogI(LogLevel.Debug, ex, message)
+        member this.LogIDebug(message: FormattableString) = this.LogI(LogLevel.Debug, message)
+
+        member this.LogIInformation(eventId: EventId, ex: Exception, message: FormattableString) =
             this.LogI(LogLevel.Information, eventId, ex, message)
-        member this.LogIInformation(eventId : EventId, message : FormattableString) =
+
+        member this.LogIInformation(eventId: EventId, message: FormattableString) =
             this.LogI(LogLevel.Information, eventId, message)
-        member this.LogIInformation(ex : Exception, message : FormattableString) =
+
+        member this.LogIInformation(ex: Exception, message: FormattableString) =
             this.LogI(LogLevel.Information, ex, message)
-        member this.LogIInformation(message : FormattableString) =
+
+        member this.LogIInformation(message: FormattableString) =
             this.LogI(LogLevel.Information, message)
 
-        member this.LogIWarning(eventId : EventId, ex : Exception,  message : FormattableString) =
+        member this.LogIWarning(eventId: EventId, ex: Exception, message: FormattableString) =
             this.LogI(LogLevel.Warning, eventId, ex, message)
-        member this.LogIWarning(eventId : EventId, message : FormattableString) =
+
+        member this.LogIWarning(eventId: EventId, message: FormattableString) =
             this.LogI(LogLevel.Warning, eventId, message)
-        member this.LogIWarning(ex : Exception, message : FormattableString) =
+
+        member this.LogIWarning(ex: Exception, message: FormattableString) =
             this.LogI(LogLevel.Warning, ex, message)
-        member this.LogIWarning(message : FormattableString) =
-            this.LogI(LogLevel.Warning, message)
 
-        member this.LogIError(eventId : EventId, ex : Exception,  message : FormattableString) =
+        member this.LogIWarning(message: FormattableString) = this.LogI(LogLevel.Warning, message)
+
+        member this.LogIError(eventId: EventId, ex: Exception, message: FormattableString) =
             this.LogI(LogLevel.Error, eventId, ex, message)
-        member this.LogIError(eventId : EventId, message : FormattableString) =
+
+        member this.LogIError(eventId: EventId, message: FormattableString) =
             this.LogI(LogLevel.Error, eventId, message)
-        member this.LogIError(ex : Exception, message : FormattableString) =
-            this.LogI(LogLevel.Error, ex, message)
-        member this.LogIError(message : FormattableString) =
-            this.LogI(LogLevel.Error, message)
 
-        member this.LogICritical(eventId : EventId, ex : Exception,  message : FormattableString) =
+        member this.LogIError(ex: Exception, message: FormattableString) = this.LogI(LogLevel.Error, ex, message)
+        member this.LogIError(message: FormattableString) = this.LogI(LogLevel.Error, message)
+
+        member this.LogICritical(eventId: EventId, ex: Exception, message: FormattableString) =
             this.LogI(LogLevel.Critical, eventId, ex, message)
-        member this.LogICritical(eventId : EventId, message : FormattableString) =
+
+        member this.LogICritical(eventId: EventId, message: FormattableString) =
             this.LogI(LogLevel.Critical, eventId, message)
-        member this.LogICritical(ex : Exception, message : FormattableString) =
+
+        member this.LogICritical(ex: Exception, message: FormattableString) =
             this.LogI(LogLevel.Critical, ex, message)
-        member this.LogICritical(message : FormattableString) =
-            this.LogI(LogLevel.Critical, message)
 
-        member this.LogI(logLevel : LogLevel, message : FormattableString) =
-            this.LogI(logLevel, 0, null, message)
-        member this.LogI(logLevel : LogLevel, eventId : EventId, message : FormattableString) =
+        member this.LogICritical(message: FormattableString) = this.LogI(LogLevel.Critical, message)
+
+        member this.LogI(logLevel: LogLevel, message: FormattableString) = this.LogI(logLevel, 0, null, message)
+
+        member this.LogI(logLevel: LogLevel, eventId: EventId, message: FormattableString) =
             this.LogI(logLevel, eventId, null, message)
-        member this.LogI(logLevel : LogLevel, ex : Exception, message : FormattableString) =
-            this.LogI(logLevel, 0, ex, message)
-        member this.LogI (logLevel : LogLevel, eventId : EventId, ex : Exception, message : FormattableString) =
-            this.Log(logLevel, eventId, (TupleLogValuesFormatter message), ex, (fun formatter _ -> formatter.ToString()))
 
-        member this.BeginScopeI(message : FormattableString) =
+        member this.LogI(logLevel: LogLevel, ex: Exception, message: FormattableString) =
+            this.LogI(logLevel, 0, ex, message)
+
+        member this.LogI(logLevel: LogLevel, eventId: EventId, ex: Exception, message: FormattableString) =
+            this.Log(
+                logLevel,
+                eventId,
+                (TupleLogValuesFormatter message),
+                ex,
+                (fun formatter _ -> formatter.ToString())
+            )
+
+        member this.BeginScopeI(message: FormattableString) =
             this.BeginScope(TupleLogValuesFormatter message)
--- tests/MEL.Flex.Tests/Tests.fs
+++ tests/MEL.Flex.Tests/Tests.fs
@@ -6,90 +6,110 @@ open Microsoft.Extensions.Logging
 open Microsoft.Extensions.Logging.Abstractions
 open MEL.Flex
 open System.Collections.Generic
+
 module Helpers =
     let kvp k v = KeyValuePair<string, obj>(k, box v)
+
 type InMemoryLogger() =
     let beginScopeCalls = Stack<obj>()
     let logCalls = ResizeArray<_>()
+
     let getStateValues state =
         match box state with
-        | :? IReadOnlyCollection<KeyValuePair<string,obj>> as s -> s :> seq<_> |> Seq.toArray
+        | :? IReadOnlyCollection<KeyValuePair<string, obj>> as s -> s :> seq<_> |> Seq.toArray
         | _ -> Array.empty
 
     member _.LogCalls = logCalls
+
     interface ILogger with
-        member this.Log<'TState>(logLevel: LogLevel, eventId: EventId, state: 'TState, ex : exn, formatter: Func<'TState,exn,string>) : unit =
-            let stateValues =
-                getStateValues state
+        member this.Log<'TState>
+            (
+                logLevel: LogLevel,
+                eventId: EventId,
+                state: 'TState,
+                ex: exn,
+                formatter: Func<'TState, exn, string>
+            ) : unit =
+            let stateValues = getStateValues state
             let message = formatter.Invoke(state, ex)
-            let scopes = beginScopeCalls |> Seq.toArray |> Array.collect getStateValues
+
+            let scopes =
+                beginScopeCalls
+                |> Seq.toArray
+                |> Array.collect getStateValues
+
             logCalls.Add(logLevel, eventId, message, stateValues, ex, scopes)
             ()
+
         member this.IsEnabled(logLevel: LogLevel) : bool = true
+
         member this.BeginScope<'TState>(state: 'TState) : IDisposable =
             beginScopeCalls.Push state
+
             { new IDisposable with
                 member _.Dispose() = beginScopeCalls.Pop() |> ignore }
 
 module TupleTests =
     open Helpers
     open MEL.Flex.Tuple
+
     [<Tests>]
     let tests =
-        testList "Tuples" [
-            testCase "No items to interpolate" <| fun _ ->
-                let logger = InMemoryLogger()
+        testList
+            "Tuples"
+            [ testCase "No items to interpolate"
+              <| fun _ ->
+                  let logger = InMemoryLogger()
 
-                let expectedState = [|
-                    kvp "{OriginalFormat}" "LOL"
-                |]
+                  let expectedState = [| kvp "{OriginalFormat}" "LOL" |]
 
-                logger.LogICritical $"""LOL"""
-                let (level, eventId, message, state, ex, scopes) = logger.LogCalls |> Seq.head
-                Expect.equal level LogLevel.Critical ""
-                Expect.equal eventId (EventId.op_Implicit 0) ""
-                Expect.equal message "LOL" ""
-                Expect.sequenceEqual state expectedState ""
-                Expect.equal ex null ""
-                Expect.equal scopes Array.empty ""
+                  logger.LogICritical $"""LOL"""
+                  let (level, eventId, message, state, ex, scopes) = logger.LogCalls |> Seq.head
+                  Expect.equal level LogLevel.Critical ""
+                  Expect.equal eventId (EventId.op_Implicit 0) ""
+                  Expect.equal message "LOL" ""
+                  Expect.sequenceEqual state expectedState ""
+                  Expect.equal ex null ""
+                  Expect.equal scopes Array.empty ""
 
-            testCase "One tuple to interpolate" <| fun _ ->
-                let logger = InMemoryLogger()
+              testCase "One tuple to interpolate"
+              <| fun _ ->
+                  let logger = InMemoryLogger()
 
-                let theConst = "UserName"
-                let theUser = "KirkJ1701"
-                let expectedState = [|
-                    kvp $"@{theConst}" theUser
-                    kvp "{OriginalFormat}" $"Some user {{@{theConst}}} logged into starship"
-                |]
+                  let theConst = "UserName"
+                  let theUser = "KirkJ1701"
 
-                logger.LogIError $"""Some user {(theConst, theUser)} logged into starship"""
+                  let expectedState =
+                      [| kvp $"@{theConst}" theUser
+                         kvp "{OriginalFormat}" $"Some user {{@{theConst}}} logged into starship" |]
 
-                let (level, eventId, message, state, ex, scopes) = logger.LogCalls |> Seq.head
-                Expect.equal level LogLevel.Error ""
-                Expect.equal eventId (EventId.op_Implicit 0) ""
-                Expect.equal message $"Some user {theUser} logged into starship" ""
-                Expect.sequenceEqual state expectedState ""
-                Expect.equal ex null ""
-                Expect.equal scopes Array.empty ""
+                  logger.LogIError $"""Some user {(theConst, theUser)} logged into starship"""
 
-            testCase "Non tuple to interpolate" <| fun _ ->
-                let logger = InMemoryLogger()
+                  let (level, eventId, message, state, ex, scopes) = logger.LogCalls |> Seq.head
+                  Expect.equal level LogLevel.Error ""
+                  Expect.equal eventId (EventId.op_Implicit 0) ""
+                  Expect.equal message $"Some user {theUser} logged into starship" ""
+                  Expect.sequenceEqual state expectedState ""
+                  Expect.equal ex null ""
+                  Expect.equal scopes Array.empty ""
 
-                let theConst = "item0"
-                let theUser = "SpockV"
-                let expectedState = [|
-                    kvp $"@{theConst}" theUser
-                    kvp "{OriginalFormat}" $"Some user {{@{theConst}}} logged into starship"
-                |]
+              testCase "Non tuple to interpolate"
+              <| fun _ ->
+                  let logger = InMemoryLogger()
 
-                logger.LogIWarning $"""Some user {theUser} logged into starship"""
+                  let theConst = "item0"
+                  let theUser = "SpockV"
 
-                let (level, eventId, message, state, ex, scopes) = logger.LogCalls |> Seq.head
-                Expect.equal level LogLevel.Warning ""
-                Expect.equal eventId (EventId.op_Implicit 0) ""
-                Expect.equal message $"Some user {theUser} logged into starship" ""
-                Expect.sequenceEqual state expectedState ""
-                Expect.equal ex null ""
-                Expect.equal scopes Array.empty ""
-        ]
+                  let expectedState =
+                      [| kvp $"@{theConst}" theUser
+                         kvp "{OriginalFormat}" $"Some user {{@{theConst}}} logged into starship" |]
+
+                  logger.LogIWarning $"""Some user {theUser} logged into starship"""
+
+                  let (level, eventId, message, state, ex, scopes) = logger.LogCalls |> Seq.head
+                  Expect.equal level LogLevel.Warning ""
+                  Expect.equal eventId (EventId.op_Implicit 0) ""
+                  Expect.equal message $"Some user {theUser} logged into starship" ""
+                  Expect.sequenceEqual state expectedState ""
+                  Expect.equal ex null ""
+                  Expect.equal scopes Array.empty "" ]
